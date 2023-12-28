e45fa84b7d4c521c44caaf7d98672464949266af
Janussr <Janusstivang@gmail.com>
2023-12-28 15:58:07 +0100

    Get review by id

--- src/Feedback.Web/Types/QueryType.cs
+++ src/Feedback.Web/Types/QueryType.cs
@@ -1,4 +1,6 @@
 ﻿using Feedback.Web.Data;
+using Feedback.Web.Entities;
+using Restaurant.Infrastructure.Interfaces;
 
 namespace Feedback.Web.Types;
 
@@ -12,6 +14,16 @@ public class QueryType : ObjectType
             {
                 var dbContext = context.Service<ReviewContext>();
                 return dbContext.Reviews.ToList();
+            });
+        
+        descriptor.Field("review")
+            .Argument("id", a => a.Type<NonNullType<IdType>>())
+            .Type<ReviewType>()
+            .Resolve(context =>
+            {
+                var reviewId = context.ArgumentValue<long>("id"); // Assuming the ID is an integer
+                var reviewRepository = context.Service<IRepository<ReviewEntity>>();
+                return reviewRepository.GetByIdAsync(reviewId); // Replace with your method to fetch a review by ID
             });
     }
 }
